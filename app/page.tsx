e7da4f359f056ed5e408d6edd99862a6e516cd0a
MuadDibGOSU <jav.kite@gmail.com>
2025-09-28 18:46:47 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      
+      <button className="text-white border-b-blue-400 bg-amber-200 rounded-4xl p-10 cursor-pointer">Click me</button>     
+    </div>
+  )
+}
+
+export default Home
